90101d059c76535c65ee30b4b677f35dc20b8af1
Jeremy Hopple <jeremy@jthopple.com>
2015-11-18 11:20:38 -0700

    Pair with Kai and Matthew to add code to read from Spark Core temperature sensor

--- lib/intro/labs/temp.ex
+++ lib/intro/labs/temp.ex
@@ -1,20 +1,36 @@
 defmodule Intro.Labs.Temp do
   def sensor_to_voltage(reading) do
-    Float.round(((reading * 3.3) / 4095), 3)
+    (reading * 3.3) / 4095
+      |> Float.round(3)
   end
 
   def voltage_to_celcius(voltage) do
-    Float.round(((voltage - 0.5) * 100), 1)
+    (voltage - 0.5) * 100
+      |> Float.round(1)
   end
 
   def celcius_to_farenheit(celcius) do
-    Float.round(((celcius * 1.8) + 32), 1)
+    (celcius * 1.8) + 32
+      |> Float.round(1)
   end
 
   def sensor_to_farenheit(reading) do
-    v = sensor_to_voltage(reading)
-    c = voltage_to_celcius(v)
-    celcius_to_farenheit(c)
+    reading
+      |> sensor_to_voltage
+      |> voltage_to_celcius
+      |> celcius_to_farenheit
+
   end
+
+  def read_from_sensor do
+    url = "https://api.spark.io/v1/devices/55ff6f065075555352271887/temperature\?access_token\=2e4be73f31cb910ded3da53275e5236b5eb129e1"
+
+    json = HTTPotion.get(url).body
+      |> Poison.decode!
+
+    reading = json["result"]
+    sensor_to_farenheit(reading)
+  end
+
 
 end
